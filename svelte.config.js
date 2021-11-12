1677efe546244bc66683598ffbebe748de6a5352
Isaac Kwok <isaac.kwok@shopee.com>
2021-11-12 15:46:55 +0800

    feat: add netlify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { windi } from 'svelte-windicss-preprocess';
 import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 
@@ -8,6 +9,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+    adapter: adapter()
 	}
 };
 
