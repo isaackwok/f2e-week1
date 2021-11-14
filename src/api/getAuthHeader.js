afc3822bf2f1eb52940be411b41aaf45eb3f698e
Isaac Kwok <isaac.kwok@shopee.com>
2021-11-14 16:38:41 +0800

    move MOTC API id and key to .env

--- src/api/getAuthHeader.js
+++ src/api/getAuthHeader.js
@@ -1,15 +1,15 @@
 import jsSHA from 'jssha';
 
 const getAuthorizationHeader = function () {
-  var AppID = '947333329b9a4c99adc478a8c12c3aa3';
-  var AppKey = 'Fuzve_DoP8ReM3KD_-12cYjq8_8';
+  const AppID = import.meta.env.VITE_MOTC_APP_ID;
+  const AppKey = import.meta.env.VITE_MOTC_APP_KEY;
 
-  var GMTString = new Date().toGMTString();
-  var ShaObj = new jsSHA('SHA-1', 'TEXT');
+  const GMTString = new Date().toGMTString();
+  const ShaObj = new jsSHA('SHA-1', 'TEXT');
   ShaObj.setHMACKey(AppKey, 'TEXT');
   ShaObj.update('x-date: ' + GMTString);
-  var HMAC = ShaObj.getHMAC('B64');
-  var Authorization = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
+  const HMAC = ShaObj.getHMAC('B64');
+  const Authorization = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
 
   return { Authorization: Authorization, 'X-Date': GMTString };
 };
