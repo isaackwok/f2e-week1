656c83c1e611d33b9dd9c20c750fca7b5f88218d
Isaac Kwok <isaac.kwok@shopee.com>
2021-11-14 17:22:03 +0800

    remove unused import in `LocationMarker`

--- src/lib/LocationMarker.svelte
+++ src/lib/LocationMarker.svelte
@@ -1,7 +1,3 @@
-<script>
-	import { add_flush_callback } from 'svelte/internal';
-</script>
-
 <div class="location-marker flex items-center leading-relaxed ">
 	<img src="/map-marker.svg" alt="" class="mr-2" />
 	<slot />
