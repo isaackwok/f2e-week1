8d30dd54d1aa9cac073c09559119b2ca3873d78f
Isaac Kwok <isaac.kwok@shopee.com>
2021-11-14 16:23:50 +0800

    feat: not to expose google map api key on git

--- src/lib/DetailPage.svelte
+++ src/lib/DetailPage.svelte
@@ -63,7 +63,7 @@
 	<div class="w-full overflow-hidden rounded-xl">
     <a href={`https://www.google.com/maps/search/${item.address}`} target="_blank">
       <img
-			src={`https://maps.googleapis.com/maps/api/staticmap?center=${item.address}&zoom=17&size=540x250&scale=2&markers=color:red|${item.address}&key=AIzaSyBH6yrrrzTxMJidGPfgtjSzSJVqpGdG9pA`}
+			src={`https://maps.googleapis.com/maps/api/staticmap?center=${item.address}&zoom=17&size=540x250&scale=2&markers=color:red|${item.address}&key=${import.meta.env.VITE_GOOGLE_STATIC_MAP_API_KEY}`}
 			alt=""
 			class="object-cover object-center h-full w-full"
       />
